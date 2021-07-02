c4c8af4fef1c081bd9f15fc50a7705462d2f964d
dsakam <dsakam@live.jp>
2021-07-03 00:57:55 +0900

    1st commit (v1.6.1)

---
+++ BUILDLet.UWP.Controls/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("BUILDLet.UWP.Controls")]
+[assembly: AssemblyDescription("BUILDLet UWP UI Control Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BUILDLet")]
+[assembly: AssemblyProduct("BUILDLet.UWP.Controls")]
+[assembly: AssemblyCopyright("© 2021 Daiki Sakamoto")]
+[assembly: AssemblyTrademark("BUILDLet")]
+[assembly: AssemblyCulture("")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+//[アセンブリ: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: ComVisible(false)]
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# BUILDLet.UWP.Controls
-BUILDLet UWP UI Control Library
+BUILDLet UWP UI Class Library
+=============================
+
+Introduction
+------------
+
+This project provides UI class library for UWP (Universal Windows Platform).
+
+Getting Started
+---------------
+
+This project is provided as the following NuGet Package(s).
+
+- [BUILDLet.UWP.Controls](https://www.nuget.org/packages/BUILDLet.UWP.Controls/)
+
+Please refer to the help document ([BUILDLet.UWP.Controls.chm](./BUILDLet.UWP.Controls.Documentation/Help/BUILDLet.UWP.Controls.chm)) for the change history and API references.
+
+Build and Test
+--------------
+
+This project (Visual Studio Solution) is built and tested on Visual Studio.
+
+License
+-------
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) License.
